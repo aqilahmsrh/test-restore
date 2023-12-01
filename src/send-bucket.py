cc725ed2a98fdfc7bf5ef2a6ec1d2aa6fd141483
aqilahmsrh <127908263+aqilahmsrh@users.noreply.github.com>
2023-12-01 16:05:50 +0800

    Update send-bucket.py

--- src/send-bucket.py
+++ src/send-bucket.py
@@ -8,6 +8,7 @@ sender_email = os.environ.get('SENDER_EMAIL')
 receiver_email = os.environ.get('RECEIVER_EMAIL')
 password = os.environ.get('PASSWORD_EMAIL')
 s3_uri = os.environ.get('S3_URI')
+env = os.environ.get('ENV')
 
 msg = MIMEMultipart()
 msg['From'] = sender_email
@@ -15,7 +16,7 @@ msg['To'] = receiver_email
 msg['Subject'] = Header('Some Title', 'utf-8').encode()
 
 # body = 'Hello World!'
-body = f"Hi Team, this is the S3 URI of the SIT and DEXSIT database dumps: {s3_uri}"
+body = f"Hi Team, this is the S3 URI of the {env} and DEX{env} database dumps: {s3_uri}"
 
 msg_content = MIMEText(body, 'plain', 'utf-8')
 msg.attach(msg_content)
