123b1219d13b379c2b962bd712fa9267da494624
aqilahmsrh <127908263+aqilahmsrh@users.noreply.github.com>
2023-12-01 15:47:50 +0800

    Update send-email.py

--- src/send-email.py
+++ src/send-email.py
@@ -6,13 +6,17 @@ from email.header import Header
 
 sender_email = os.environ.get('SENDER_EMAIL')
 receiver_email = os.environ.get('RECEIVER_EMAIL')
+receiver_email_two = os.environ.get('RECEIVER_EMAIL_TWO')
 password = os.environ.get('PASSWORD_EMAIL')
 requested_user = os.environ.get('REQUESTED_USER')
 workflow_link = os.environ.get('WORKFLOW_LINK')
 
+receiver_emails = [os.environ.get('RECEIVER_EMAIL'), os.environ.get('RECEIVER_EMAIL_TWO')]
+
 msg = MIMEMultipart()
 msg['From'] = sender_email
-msg['To'] = receiver_email
+msg['To'] = ', '.join(receiver_emails)
+# msg['To'] = receiver_email
 msg['Subject'] = Header('Some Title', 'utf-8').encode()
 
 body = f"Hi DevOps, your SIT MySQL DUmp is triggered; {workflow_link} by {requested_user}"
@@ -24,4 +28,4 @@ msg.attach(msg_content)
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.sendmail(sender_email, receiver_emails, msg.as_string())
