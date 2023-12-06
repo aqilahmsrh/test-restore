c75a4f2217a3acf8078b7064825e5a94ee9ddf33
aqilahmsrh <127908263+aqilahmsrh@users.noreply.github.com>
2023-12-06 10:56:35 +0800

    Update send-email.py

--- src/send-email.py
+++ src/send-email.py
@@ -5,19 +5,19 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 sender_email = os.environ.get('SENDER_EMAIL')
-receiver_email = os.environ.get('RECEIVER_EMAIL')
-receiver_email_two = os.environ.get('RECEIVER_EMAIL_TWO')
+# receiver_email = os.environ.get('RECEIVER_EMAIL')
+# receiver_email_two = os.environ.get('RECEIVER_EMAIL_TWO')
 password = os.environ.get('PASSWORD_EMAIL')
 requested_user = os.environ.get('REQUESTED_USER')
 workflow_link = os.environ.get('WORKFLOW_LINK')
 env = os.environ.get('ENV')
 
-# receiver_emails = [os.environ.get('RECEIVER_EMAIL'), os.environ.get('RECEIVER_EMAIL_TWO')]
+receiver_emails = [os.environ.get('RECEIVER_EMAIL'), os.environ.get('RECEIVER_EMAIL_TWO')]
 
 msg = MIMEMultipart()
 msg['From'] = sender_email
-# msg['To'] = ', '.join(receiver_emails)
-msg['To'] = receiver_email
+msg['To'] = ', '.join(receiver_emails)
+# msg['To'] = receiver_email
 msg['Subject'] = Header('Database Backup Request', 'utf-8').encode()
 
 body = f"""
@@ -38,4 +38,4 @@ msg.attach(msg_content)
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.sendmail(sender_email, receiver_emails, msg.as_string())
